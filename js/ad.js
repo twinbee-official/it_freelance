db826d9a46cc56c7e4d38f45d226c4d8223c9d6e
Hiramoto Sayaka <s.hiramoto@twinbee.co.jp>
2023-09-12 00:51:20 +0900

    Twinbeeにできることの+と-修正。

--- js/ad.js
+++ js/ad.js
@@ -4,10 +4,11 @@ $(function() {
 		e.preventDefault();
 		if ($(this).prev().is(':hidden')) {
 			$(this).prev().slideDown();
-			$(this).find('.button').text('閉じる').addClass('boxclose').removeClass('boxopen');
+			$(this).find('.button').text('- タップ閉じる').addClass('boxclose').removeClass('boxopen');
 		} else {
 			$(this).prev().slideUp();
-			$(this).find('.button').text('タップで詳しくみる').removeClass('boxclose').addClass('boxopen');
+			$(this).find('.button').text('＋ タップで詳しくみる').removeClass('boxclose').addClass('boxopen');
 		}
 	});
 });
+
